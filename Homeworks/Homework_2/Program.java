362900f405f16dc20156504d4104a2f43c3e035e
Aidar Shaidullov <ashaidullov17gmail.com>
2019-04-24 00:52:48 +0300

    add Program.java file

---
+++ Homeworks/Homework_2/Program.java
@@ -0,0 +1,26 @@
+class Program {
+	public static void main(String[] args){
+		int x5000, x2000, x1000, x500, x200, x100, x50, x10, x5, x2;
+		int number = 13765;
+		x5000 = number / 5000;
+		x2000 = number / 2000;
+		x1000 = number / 1000;
+		x500 = number / 500;
+		x200 = number / 200;
+		x100 = number / 100;
+		x50 = number / 50;
+		x10 = number / 10;
+		x5 = number / 5;
+		x2 = number / 2;
+
+System.out.println(x5000); 
+System.out.println(x2000); 
+System.out.println(x1000);
+System.out.println(x500);
+System.out.println(x100);
+System.out.println(x50);
+System.out.println(x10);
+System.out.println(x5);
+System.out.println(x2);
+	}
+}
