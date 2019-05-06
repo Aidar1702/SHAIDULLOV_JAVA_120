ccae10ef77e49f0cfbaf3c9f4e592615e4bffe4e
Aidar Shaidullov <ashaidullov17gmail.com>
2019-05-06 17:43:38 +0300

    add Program.java file

--- Homeworks/Homework_4/Program.java
+++ Homeworks/Homework_4/Program.java
@@ -1,38 +1,26 @@
 import java.util.Scanner;
-public class Program {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int currentDigit = 0;
-        int Digit = 0;
-        int[]arr = new int[10];
-
-        while (currentDigit != -1) {
-            currentDigit = scanner.nextInt();
-
-            for (;currentDigit > 0; currentDigit /= 10){
-                Digit = currentDigit % 10;
-                arr[Digit]++;// arr[Digit]+1
-                while (currentDigit <= 0) {
-                    currentDigit = scanner.nextInt();
-
-                    for (;currentDigit > 0; currentDigit /= 10){
-                        Digit = currentDigit % 10;
-                        arr[Digit]++;// arr[Digit]+1
-                    }
-                }
-            }
-        }
-        System.out.println("numberOne " + arr[1]);
-        System.out.println("numberTwo " + arr[2]);
-        System.out.println("numberThree " + arr[3]);
-        System.out.println("numberFour " + arr[4]);
-        System.out.println("numberFive " + arr[5]);
-        System.out.println("numberSix " + arr[6]);
-        System.out.println("numberSeven " + arr[7]);
-        System.out.println("numberEight " + arr[8]);
-        System.out.println("numberNine " + arr[9]);
-
-        scanner.close();
-    }
+class Program {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int array[] = new int[10];
+	int currentDigit = 0;
+	int currentNumber = scanner.nextInt();
+		while (currentNumber != -1) {
+			while (currentNumber != 0) {
+				currentDigit = currentNumber % 10;
+					for (int i = 1; i < 10; i++) {
+						if (currentDigit == i) {
+							array[i] = array[i] + 1;
+						}
+					}
+				currentNumber = currentNumber / 10;	
+			}
+		currentNumber = scanner.nextInt();	
+		}
+	for (int i = 1; i < 10; i++) {
+		if (array[i] != 0) {
+		System.out.println("Digit " + i + " = " + array[i]);	
+		}
+	}
+	}
 }
